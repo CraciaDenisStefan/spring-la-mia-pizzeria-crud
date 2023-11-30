5909e25e7c3b6e838c0ca5634cc9d55ef2f0b366
CraciDenisStefan <crdenis13@gmail.com>
2023-11-30 15:23:32 +0100

    Mostrate pizze in index

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -1,5 +1,37 @@
 package org.java.spring.controller;
 
+import java.util.List;
+import org.java.spring.db.pojo.Pizzeria;
+import org.java.spring.db.serve.Pizzeriaservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class MainController {
 
+	@Autowired
+	private Pizzeriaservice pizzeriaService;
+	
+	@GetMapping("/")
+	public String getPizze(Model model) {
+		
+		List<Pizzeria> pizze = pizzeriaService.findAll();
+		
+		model.addAttribute("pizze", pizze);
+		
+		return "index";
+	}
+	
+	@GetMapping("/pizza/{id}")
+	public String getPizza(Model model,
+			@PathVariable int id) {
+		
+		Pizzeria pizza = pizzeriaService.findById(id);
+		model.addAttribute("pizza", pizza);
+		
+		return "pizza";
+	}
 }
